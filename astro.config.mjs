5d2f00096b0cb25f60b43e4028881e7f557c7b68
gavrielc <gavriel@Gavriels-MacBook-Pro-2.local>
2023-02-13 17:17:18 +0200

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import analytics from "astro-analytics";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [analytics()]
+  integrations: []
 });
