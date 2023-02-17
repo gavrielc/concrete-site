0fdb7a8be063815b35fdf5b42613123bc0b2f366
gavrielc <gavriel@Gavriels-MacBook-Pro-2.local>
2023-02-17 21:50:37 +0200

    images

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://concrete.media/',
-  integrations: [sitemap()]
+  integrations: [sitemap(), image()]
 });
