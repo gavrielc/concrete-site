bb1f18f2711562c5daa45bfd83fe33257335e2de
gavrielc <gavriel@Gavriels-MacBook-Pro-2.local>
2023-02-24 21:49:45 +0200

    filtering

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://concrete.media/',
-  integrations: [sitemap()]
+  integrations: [sitemap(), preact()]
 });
---
+++ src/components/Clients.jsx
@@ -0,0 +1,112 @@
+import {useState, useCallback} from 'preact/hooks';
+
+import qm from '../assets/client-logos/QM.png';
+import buildots from '../assets/client-logos/Buildots.webp';
+import ox from '../assets/client-logos/Ox.png';
+import pinecone from '../assets/client-logos/Pinecone.svg';
+import runai from '../assets/client-logos/Runai.svg';
+import wilco from '../assets/client-logos/Wilco.png';
+import aporia from '../assets/client-logos/Aporia.svg';
+import cipia from '../assets/client-logos/Cipia.png';
+import spectral from '../assets/client-logos/Spectral.svg';
+import imperium from '../assets/client-logos/Imperium Jets.png';
+import armo from '../assets/client-logos/Armo.png';
+import port from '../assets/client-logos/Port.svg';
+import aidoc from '../assets/client-logos/Aidoc.png';
+import scopio from '../assets/client-logos/Scopio.png';
+import lumigo from '../assets/client-logos/Lumigo.svg';
+import privya from '../assets/client-logos/Privya.svg';
+import arnica from '../assets/client-logos/Arnica.webp';
+import rookout from '../assets/client-logos/Rookout.png';
+import sayata from '../assets/client-logos/Sayata.png';
+import imperva from '../assets/client-logos/Imperva.png';
+import swimm from '../assets/client-logos/Swimm.svg';
+import explorium from '../assets/client-logos/Explorium.svg';
+import jit from '../assets/client-logos/Jit.svg';
+import tatio from '../assets/client-logos/Tatio.png';
+import safeguard from '../assets/client-logos/Safeguard.png';
+import masterschool from '../assets/client-logos/Masterschool.webp';
+import frontegg from '../assets/client-logos/Frontegg.svg';
+import nexar from '../assets/client-logos/Nexar.png';
+import storai from '../assets/client-logos/Storai.png';
+import datree from '../assets/client-logos/Datree.png';
+import anima from '../assets/client-logos/Anima.png';
+import bright from '../assets/client-logos/Bright.png';
+import cybermdx from '../assets/client-logos/Cybermdx.png';
+import dia from '../assets/client-logos/DIA.webp';
+import dig from '../assets/client-logos/Dig.svg';
+import lightrun from '../assets/client-logos/Lightrun.png';
+import stoke from '../assets/client-logos/Stoke.svg';
+import ubeya from '../assets/client-logos/Ubeya.svg';
+import voom from '../assets/client-logos/Voom.png';
+
+const clients = [
+    {logo: jit, name: "jit", site: 'https://www.jit.io/', tags: ['security', 'dev', 'saas']},
+    {logo: qm, name: "qm", site: 'https://www.quantum-machines.co/', tags: ['deep tech']},
+    {logo: ox, name: "ox", site: 'https://www.ox.security/', tags: ['security', 'saas']},
+    {logo: runai, name: "runai", site: 'https://www.run.ai/', tags: ['AI & ML', 'saas']},
+    {logo: aidoc, name: "aidoc", site: 'https://www.aidoc.com/', tags: ['medtech', 'AI & ML']},
+    {logo: pinecone, name: "pinecone", site: 'https://www.pinecone.io/', tags: ['AI & ML', 'saas']},
+    {logo: aporia, name: "aporia", site: 'https://www.aporia.com/', tags: ['AI & ML', 'saas']},
+    {logo: buildots, name: "buildots", site: 'https://buildots.com/', tags: ['AI & ML']},
+    {logo: cipia, name: "cipia", site: 'https://cipia.com/', tags: ['AI & ML']},
+    {logo: armo, name: "armo", site: 'https://www.armosec.io/', tags: ['dev', 'security']},
+    {logo: wilco, name: "wilco", site: 'https://www.trywilco.com/', tags: ['dev']},
+    {logo: scopio, name: "scopio", site: 'https://scopiolabs.com/', tags: ['medtech']},
+    {logo: lumigo, name: "lumigo", site: 'https://www.lumigo.io/', tags: ['dev', 'saas']},
+    {logo: privya, name: "privya", site: 'https://privya.ai/', tags: ['security']},
+    {logo: arnica, name: "arnica", site: 'https://www.arnica.io/', tags: ['security', 'dev', 'saas']},
+    {logo: rookout, name: "rookout", site: 'https://www.rookout.com/', tags: ['dev', 'saas']},
+    {logo: sayata, name: "sayata", site: 'https://www.sayatalabs.com/', tags: ['insurtech']},
+    {logo: imperva, name: "imperva", site: 'https://www.imperva.com/', tags: ['security', 'saas']},
+    {logo: swimm, name: "swimm", site: 'https://swimm.io/', tags: ['dev', 'saas']},
+    {logo: explorium, name: "explorium", site: 'https://www.explorium.ai/', tags: ['AI & ML', 'saas']},
+    {logo: tatio, name: "tatio", site: 'https://www.tatio.io/', tags: ['saas']},
+    {logo: port, name: "port", site: 'https://www.getport.io/', tags: ['dev', 'saas']},
+    {logo: safeguard, name: "safeguard", site: 'https://en.safeguard.co.il/', tags: []},
+    {logo: masterschool, name: "masterschool", site: 'https://www.masterschool.com/', tags: []},
+    {logo: frontegg, name: "frontegg", site: 'https://frontegg.com/', tags: ['dev', 'saas']},
+    {logo: nexar, name: "nexar", site: 'https://www.getnexar.com/', tags: ['AI & ML']},
+    {logo: storai, name: "storai", site: 'https://stor.ai/', tags: []},
+    {logo: datree, name: "datree", site: 'https://www.datree.io/', tags: ['security', 'dev', 'saas']},
+    {logo: spectral, name: "spectral", site: 'https://spectralops.io/', tags: ['dev', 'security', 'saas']},
+    {logo: dig, site: 'https://www.dig.security/', tags: ['security', 'saas']},
+    {logo: voom, site: 'https://www.voominsurance.com/', tags: ['insurtech']},
+    {logo: dia, site: 'https://www.dia-analysis.com/', tags: ['medtech']},
+    {logo: cybermdx, site: 'https://www.cybermdx.com/', tags: ['security', 'medtech']},
+    {logo: anima, site: 'https://www.animaapp.com/', tags: ['dev', 'saas']},
+    {logo: ubeya, site: 'https://www.ubeya.com/', tags: ['saas']},
+    {logo: stoke, site: 'https://www.stoke.world/', tags: ['saas']},
+    {logo: imperium, name: "imperium", site: 'https://www.imperiumjets.com/', tags: []},
+    {logo: lightrun, site: 'https://lightrun.com/', tags: ['dev', 'saas']},
+    {logo: bright, site: 'https://brightsec.com/', tags: ['dev', 'security', 'saas']},
+];
+
+const tags = [
+    {name: 'Developer Tools', value: 'dev'},
+    {name: 'CyberSecurity', value: 'security'},
+    {name: 'AI & ML', value: 'AI & ML'},
+    {name: 'Medtech', value: 'medtech'},
+    {name: 'B2B SaaS', value: 'saas'},
+    {name: 'Insurtech', value: 'insurtech'},
+    {name: 'Deep Tech', value: 'deep tech'}
+];
+
+export default function Clients() {
+    const [value, setValue] = useState();
+    const filter = useCallback((tag) => {
+        const newValue = value == tag ? null : tag;
+        setValue(newValue);
+    }, [value]);
+
+    return (
+        <>
+            <div className='tags'>
+                {tags.map(({name, value: val}) => <button className={'tag'.concat(value == val ? ' active' : '')} onClick={() => filter(val)}>{name}</button>)}
+            </div>
+            <div className='logos-wrapper'>
+                {clients.filter(({tags}) => !value || tags.includes(value)).map(({logo, site, name}) => <a href={site} target="_blank"><img class="logo" src={logo} alt={`${name} logo`}/></a>)}
+            </div>
+        </>
+    );
+}
