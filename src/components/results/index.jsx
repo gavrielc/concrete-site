f1c456c68e7e596a426211a338e38265ef1be05a
gavrielc <gavriel@Gavriels-MacBook-Pro-2.local>
2023-03-11 14:30:09 +0200

    results

---
+++ src/components/results/index.jsx
@@ -0,0 +1,39 @@
+import {useState} from 'preact/hooks';
+import styles from './styles.module.scss';
+import classNames from 'classnames/bind';
+import results from './results';
+
+const cn = classNames.bind(styles);
+
+const tags = [
+    {name: 'Highlights', value: 'highlights'},
+    {name: 'Developer Tools', value: 'dev'},
+    {name: 'CyberSecurity', value: 'security'},
+    {name: 'AI & ML', value: 'AI & ML'},
+    {name: 'Medtech', value: 'medtech'},
+    {name: 'B2B SaaS', value: 'saas'},
+    {name: 'HR Tech', value: 'hr'}
+];
+
+export default function Results() {
+    const [value, setValue] = useState('highlights');
+
+    return (
+        <>
+            <div className={styles.tags}>
+                {tags.map(({name, value: val}) => <button className={cn('tag', {active: value == val})} onClick={() => setValue(val)}>{name}</button>)}
+            </div>
+            <div className={styles.resultsWrapper}>
+                {results.filter(({tags}) => !value || tags.includes(value)).map(({articleUrl, logo, headline, publication, date}) => (
+                <li className={styles["result-card"]}>
+                    <a href={articleUrl} target="_blank">
+                        <img src={logo} alt={`${publication} logo`}/>
+                        <h4>{headline}</h4>
+                        <p>{date}</p>
+                    </a>
+                </li>
+))}
+            </div>
+        </>
+    );
+}
---
+++ src/components/results/results.js
@@ -0,0 +1,321 @@
+import techcrunch from '../../assets/publication-logos/techcrunch.png';
+import forbes from '../../assets/publication-logos/forbes.webp';
+import mit from '../../assets/publication-logos/mit.webp';
+import vb from '../../assets/publication-logos/vb.svg';
+import techtarget from '../../assets/publication-logos/techtarget.png';
+import ctech from '../../assets/publication-logos/ctech.png';
+import tns from '../../assets/publication-logos/tns.svg';
+import siliconangle from '../../assets/publication-logos/siliconangle.png';
+import sdtimes from '../../assets/publication-logos/sdtimes.png';
+import labiotech from '../../assets/publication-logos/labiotech.png';
+import healthcare from '../../assets/publication-logos/healthcare.png';
+import darkreading from '../../assets/publication-logos/dark-reading.png';
+import sdxcentral from '../../assets/publication-logos/sdxcentral.svg';
+import securityboulevard from '../../assets/publication-logos/security-boulevard.png';
+import containerjournal from '../../assets/publication-logos/container-journal.png';
+import scmagazine from '../../assets/publication-logos/scmagazine.png';
+import computerweekly from '../../assets/publication-logos/computerweekly.jpeg';
+import fastcompany from '../../assets/publication-logos/fast-company.png';
+import cbs from '../../assets/publication-logos/cbs.png';
+import newscientist from '../../assets/publication-logos/new-scientist.png';
+import register from '../../assets/publication-logos/register.png';
+
+export default [
+	{
+		articleUrl: 'https://www.fastcompany.com/90848766/most-innovative-companies-medical-devices-2023',
+		logo: fastcompany,
+		publication: 'Fast Company',
+		date: 'March 2, 2023',
+		headline: 'The 10 most innovative medical device companies of 2023',
+        tags: ['highlights', 'medtech']
+	},
+	{
+		articleUrl: 'https://techcrunch.com/2023/03/01/qwak-raises-12m-for-its-mlops-platform/',
+		logo: techcrunch,
+		publication: 'TechCrunch',
+		date: 'March 1, 2023',
+		headline: 'Qwak raises $12M for its MLOps platform',
+        tags: ['highlights', 'AI & ML', 'saas']
+	},
+	{
+		articleUrl: 'https://venturebeat.com/ai/qwak-an-all-in-one-mlops-platform-to-build-and-deploy-models-raises-12m/',
+		logo: vb,
+		publication: 'VentureBeat',
+		date: 'March 1, 2023',
+		headline: 'Qwak, an all-in-one MLOps platform to build and deploy models, raises $12M',
+        tags: ['highlights', 'AI & ML', 'saas']
+	},
+	{
+		articleUrl: 'https://techcrunch.com/2023/02/28/gable-series-a/',
+		logo: techcrunch,
+		publication: 'TechCrunch',
+		date: 'February 28, 2023',
+		headline: 'Gable raises the roof (and $12M) for better remote working options',
+        tags: ['highlights', 'hr', 'saas']
+	},
+	{
+		articleUrl: 'https://www.darkreading.com/dr-tech/how-to-reduce-code-risk-using-pipelineless-security',
+		logo: darkreading,
+		publication: 'DarkReading',
+		date: 'February 27, 2023',
+		headline: 'How to Reduce Code Risk Using Pipelineless Security',
+        tags: ['security', 'dev']
+	},
+	{
+		articleUrl: 'https://www.forbes.com/sites/gilpress/2023/02/16/what-chatgpt-and-generative-ai-mean-for-your-business/',
+		logo: forbes,
+		publication: 'Forbes',
+		date: 'February 16, 2023',
+		headline: 'What ChatGPT And Generative AI Mean For Your Business?',
+        tags: ['highlights', 'AI & ML', 'saas']
+	},
+	{
+		articleUrl: 'https://www.technologyreview.com/2023/02/16/1068695/chatgpt-chatbot-battle-search-microsoft-bing-google/',
+		logo: mit,
+		publication: 'MIT Technology Review',
+		date: 'February 16, 2023',
+		headline: 'The ChatGPT-fueled battle for search is bigger than Microsoft or Google',
+        tags: ['highlights', 'AI & ML']
+	},
+	{
+		articleUrl: 'https://venturebeat.com/data-infrastructure/what-are-the-dos-and-donts-for-success-with-low-code-no-code-platforms/',
+		logo: vb,
+		publication: 'VentureBeat',
+		date: 'February 15, 2023',
+		headline: 'What are the dos and don’ts for success with low-code/no-code platforms?',
+        tags: ['highlights', 'AI & ML', 'dev']
+	},
+	{
+		articleUrl: 'https://www.computerweekly.com/news/365531024/OSCR-framework-to-stop-supply-chain-attacks-in-the-wild',
+		logo: computerweekly,
+		publication: 'Computer Weekly',
+		date: 'February 14, 2023',
+		headline: 'OSC&R framework to stop supply chain attacks in the wild',
+        tags: ['highlights', 'dev', 'security']
+	},
+	{
+		articleUrl: 'https://www.calcalistech.com/ctechnews/article/98yjyqqb1',
+		logo: ctech,
+		publication: 'CTech',
+		date: 'February 12, 2023',
+		headline: 'Extreme Makover: “Generative AI is the most exciting step in AI”',
+        tags: ['AI & ML']
+	},
+	{
+		articleUrl: 'https://venturebeat.com/security/armo-shows-how-chatgpt-can-help-protect-kubernetes/',
+		logo: vb,
+		publication: 'VentureBeat',
+		date: 'February 7, 2023',
+		headline: 'ARMO shows how ChatGPT can help protect Kubernetes ',
+        tags: ['highlights', 'security']
+	},
+	{
+		articleUrl: 'https://www.sdxcentral.com/articles/news/armo-integrates-chatgpt-for-cloud-native-security/2023/02/',
+		logo: sdxcentral,
+		publication: 'SDXCentral',
+		date: 'February 7, 2023',
+		headline: 'Armo Integrates ChatGPT for Cloud-Native Security',
+        tags: ['security']
+	},
+	{
+		articleUrl: 'https://www.darkreading.com/application-security/armo-integrates-chatgpt-to-help-users-secure-kubernetes',
+		logo: darkreading,
+		publication: 'DarkReading',
+		date: 'February 7, 2023',
+		headline: 'ARMO Integrates ChatGPT to Help Users Secure Kubernetes',
+        tags: ['security']
+	},
+	{
+		articleUrl: 'https://thenewstack.io/when-losing-10000-to-cryptojacking-is-a-good-investment/',
+		logo: tns,
+		publication: 'TheNewStack',
+		date: 'February 6, 2023',
+		headline: 'When Losing $10,000 to Cryptojacking Is a Good Investment',
+        tags: ['dev']
+	},
+	{
+		articleUrl: 'https://www.theregister.com/2023/02/05/supply_chain_security_efforts/',
+		logo: register,
+		publication: 'The Register',
+		date: 'February 5, 2023',
+		headline: 'Have we learned anything from SolarWinds supply chain attacks?',
+        tags: ['highlights', 'security', 'dev']
+	},
+	{
+		articleUrl: 'https://techcrunch.com/2023/02/01/gem-security-wants-to-secure-your-cloud-infrastructure-raises-11m/',
+		logo: techcrunch,
+		publication: 'TechCrunch',
+		date: 'February 1, 2023',
+		headline: 'Gem Security wants to secure your cloud infrastructure, raises $11M',
+        tags: ['highlights', 'dev', 'security']
+	},
+	{
+		articleUrl: 'https://venturebeat.com/security/gem-security-shows-detection-and-response-key-to-cloud-security-raises-11m/',
+		logo: vb,
+		publication: 'VentureBeat',
+		date: 'February 1, 2023',
+		headline: 'Gem Security shows detection and response key to cloud security, raises $11M',
+        tags: ['highlights', 'dev', 'security']
+	},
+	{
+		articleUrl: 'https://siliconangle.com/2023/02/01/israeli-cloud-security-startup-gem-security-launches-11m-funding/',
+		logo: siliconangle,
+		publication: 'SiliconAngle',
+		date: 'February 1, 2023',
+		headline: 'Israeli cloud security startup Gem Security launches with $11M in funding',
+        tags: ['dev', 'security']
+	},
+	{
+		articleUrl: 'https://securityboulevard.com/2023/01/kubernetes-security-and-misconfigurations-with-jimmy-mesta/',
+		logo: securityboulevard,
+		publication: 'Security Boulevard',
+		date: 'January 27, 2023',
+		headline: 'Kubernetes Security And Misconfigurations With Jimmy Mesta',
+        tags: ['security']
+	},
+	{
+		articleUrl: 'https://containerjournal.com/features/why-you-shouldnt-use-a-jump-server-to-protect-kubernetes-api/',
+		logo: containerjournal,
+		publication: 'Container Journal',
+		date: 'January 26, 2023',
+		headline: 'Why You Shouldn’t Use a Jump Server to Protect Kubernetes’ API',
+        tags: ['security']
+	},
+	{
+		articleUrl: 'https://www.labiotech.eu/interview/scopio-future-telehematology/',
+		logo: labiotech,
+		publication: 'LABIOTECH',
+		date: 'January 26, 2023',
+		headline: 'Scopio on the future of telehematology',
+        tags: ['medtech']
+	},
+	{
+		articleUrl: 'https://thenewstack.io/how-jit-builds-a-platform-engineering-mindset-in-the-cloud/',
+		logo: tns,
+		publication: 'TheNewStack',
+		date: 'January 25, 2023',
+		headline: 'How Jit Builds a Platform Engineering Mindset in the Cloud',
+        tags: ['dev', 'security']
+	},
+	{
+		articleUrl: 'https://techcrunch.com/2023/01/18/quantum-machines-continues-to-grow-in-spite-of-economic-uncertainity/',
+		logo: techcrunch,
+		publication: 'TechCrunch',
+		date: 'January 18, 2023',
+		headline: 'Quantum Machines continues to grow in spite of economic uncertainity',
+        tags: ['highlights']
+	},
+	{
+		articleUrl: 'https://www.scmagazine.com/brief/cloud-security/kubescape-accepted-into-cloud-native-computing-foundation',
+		logo: scmagazine,
+		publication: 'SC Magazine',
+		date: 'January 17, 2023',
+		headline: 'Kubescape accepted into Cloud Native Computing Foundation',
+        tags: ['security']
+	},
+	{
+		articleUrl: 'https://www.computerweekly.com/blog/Open-Source-Insider/Armo-goes-CNCF-sandbox-wider-platform-offering-emerges',
+		logo: computerweekly,
+		publication: 'Computer Weekly',
+		date: 'January 16, 2023',
+		headline: 'Armo goes CNCF sandbox, wider platform offering emerges',
+        tags: ['highlights', 'dev', 'security']
+	},
+	{
+		articleUrl: 'https://healthcare-digital.com/technology-and-ai/itai-hayut-ceo-of-scopio-labs-on-ai-blood-cancer',
+		logo: healthcare,
+		publication: 'Healthcare Digital',
+		date: 'January 13, 2023',
+		headline: 'Itai Hayut, CEO of Scopio Labs, on AI & blood cancer',
+        tags: ['medtech']
+	},
+	{
+		articleUrl: 'https://thenewstack.io/kubescape-a-cncf-sandbox-platform-for-all-kubernetes-security/',
+		logo: tns,
+		publication: 'TheNewStack',
+		date: 'January 12, 2023',
+		headline: 'Kubescape: A CNCF Sandbox Platform for All Kubernetes Security',
+        tags: ['dev', 'security']
+	},
+	{
+		articleUrl: 'https://siliconangle.com/2023/01/11/armos-kubernetes-security-platform-kubescape-becomes-cncf-sandbox-project/',
+		logo: siliconangle,
+		publication: 'Silicon Angle',
+		date: 'January 11, 2023',
+		headline: 'Armo’s Kubernetes security platform Kubescape becomes a CNCF sandbox project',
+        tags: ['dev', 'security']
+	},
+	{
+		articleUrl: 'https://www.techtarget.com/searchenterpriseai/tip/9-top-AI-and-machine-learning-trends',
+		logo: techtarget,
+		publication: 'TechTarget',
+		date: 'January 9, 2023',
+		headline: '10 top AI and machine learning trends for 2023',
+        tags: ['highlights', 'AI & ML']
+	},
+	{
+		articleUrl: 'https://www.newscientist.com/article/2353610-ai-could-predict-hurricanes-or-rogue-waves-from-minimal-data/',
+		logo: newscientist,
+		publication: 'New Scientist',
+		date: 'January 6, 2023',
+		headline: 'AI could predict hurricanes or rogue waves from minimal data',
+        tags: ['highlights', 'AI & ML']
+	},
+	{
+		articleUrl: 'https://www.cbsnews.com/atlanta/video/the-breakdown-looking-at-the-future-of-building-construction/#x',
+		logo: cbs,
+		publication: 'CBS',
+		date: 'December 27, 2022',
+		headline: 'The Breakdown: Looking at the future of building construction',
+        tags: ['highlights', 'AI & ML']
+	},
+	{
+		articleUrl: 'https://thenewstack.io/beyond-hello-world-startup-gamifies-development-skills/',
+		logo: tns,
+		publication: 'TheNewStack',
+		date: 'December 12, 2022',
+		headline: 'Beyond Hello World: Startup Gamifies Development Skills',
+        tags: ['dev']
+	},
+	{
+		articleUrl: 'https://sdtimes.com/kubernetes/security-platform-kubescape-accepted-into-cncf-sandbox/',
+		logo: sdtimes,
+		publication: 'SDTimes',
+		date: 'December 11, 2022',
+		headline: 'Security platform Kubescape accepted into CNCF Sandbox',
+        tags: ['dev', 'security']
+	},
+	{
+		articleUrl: 'https://siliconangle.com/2022/12/06/wilco-opens-gamified-training-platform-dev-tool-makers/',
+		logo: siliconangle,
+		publication: 'SiliconAngle',
+		date: 'December 6, 2022',
+		headline: 'Wilco opens its gamified training platform to developer tool makers',
+        tags: ['dev']
+	},
+	{
+		articleUrl: 'https://www.forbes.com/sites/adrianbridgwater/2022/12/06/israeli-software-engineering-start-up-creates-simulation-training-quests/',
+		logo: forbes,
+		publication: 'Forbes',
+		date: 'December 6, 2022',
+		headline: 'Israeli Software Engineering Start-Up Creates Simulation Training ‘Quests’',
+        tags: ['highlights', 'dev']
+	},
+	{
+		articleUrl: 'https://sdtimes.com/software-development/wilco-releases-quest-builder-for-companies-to-showcase-their-development-tools/',
+		logo: sdtimes,
+		publication: 'SDTimes',
+		date: 'December 6, 2022',
+		headline: 'Wilco releases Quest Builder for companies to showcase their development tools',
+        tags: ['dev']
+	},
+	{
+		articleUrl: 'https://www.calcalistech.com/ctechnews/article/gr6j2q984',
+		logo: ctech,
+		publication: 'CTech',
+		date: 'November 6, 2022',
+		headline: 'Not under the microscope: Scopio Labs transforms blood testing',
+        tags: ['medtech']
+	},
+	
+];
